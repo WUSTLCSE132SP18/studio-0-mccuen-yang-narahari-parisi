d0ca5ad13e5cae0097d894bf49f38ece37bd6e0c
ReidParisi <ReidParisi@computers-mbp.dhcp.wustl.edu>
2018-01-17 15:34:11 -0600

    studio 0

--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -7,7 +7,22 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello, world!");
+  pinMode(13,OUTPUT);
+  int sec =0;
 }
 
 void loop() {
+//  for(int s =0; s<20; ++s){
+//  delay(1000);
+//  Serial.print(s);
+//  Serial.println(" second have elapsed");
+delay(1000);
+digitalWrite(13,HIGH);
+Serial.println(millis());
+delay(1000);
+digitalWrite(13,LOW);
+Serial.println(millis());
+
+//}
 }
+
---
+++ src/Heartbeat.java
@@ -0,0 +1,23 @@
+
+public class Heartbeat {
+
+	public static void main(String[] args) {
+		int s =0;
+	while(true) {	
+	try {
+	
+		Thread.sleep(1000);
+		s=s+1;
+		
+		System.out.println(s + " second have elapsed");
+	} catch (InterruptedException e) {
+	
+		e.printStackTrace();
+	}
+	}	
+	
+	
+
+	}
+
+}
